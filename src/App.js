c8bcd8b1ec7473069e15ce2587475dc63314d1a6
joker-jonesy <spashmcgrath94@yahoo.com>
2022-10-13 10:58:27 -0400

    Kermit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const [munch, setMunch]=React.useState(3);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cookie" onClick={()=>setMunch(munch-1)}>
+          {munch>=3&&<div className="munch">1</div>}
+          {munch>=2&&<div className="munch">2</div>}
+          {munch>=1&&<div className="munch">3</div>}
+      </div>
     </div>
   );
 }
